bd059085a97bfafc6eff07d146a394e91206645d
Julia Margarita Jimenez Herrera <maggie.jh99@gmail.com>
2020-05-27 17:55:25 -0500

    Carrito Model

--- scr/models/carritoModel.js
+++ scr/models/carritoModel.js
@@ -9,6 +9,10 @@ const carritoSchema = mongoose.Schema({
             type : String,
             requiered : true
         },
+        cantidad : {
+            type : Number,
+            requiered : true
+        }, 
         precioInd : {
             type : Number,
             requiered : true
@@ -33,11 +37,31 @@ mongoose.pluralize(null);
 const carritosCollection = mongoose.model( 'Carrito', carritoSchema );
 
 const Carrito = {
-    addNuevoProducto : function( newPedido ){
-        return pedidosCollection
-        .create( newPedido )
-        .then( pedidoCreated => {
-            return pedidoCreated;
+    createNewCarrito : function( newCarrito ){
+        return carritosCollection
+        .create( newCarrito )
+        .then( carritoCreated => {
+            return carritoCreated;
+        })
+        .catch( err => {
+            return err;
+        })
+    },
+    deleteCarrito : function( email ){
+        return carritosCollection
+        .deleteOne({ email : email })
+        .then( results => {
+            return results;
+        })
+        .catch( err => {
+            return err;
+        })
+    },
+    addProductToCarrito : function( newProducto ){
+        return carritosCollection
+        .updateOne({email : email}, { $push: {productos : newProducto}})
+        .then( results => {
+            return results;
         })
         .catch( err => {
             return err;
